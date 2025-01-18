a8da59264f2baeade73c396b441c6d6f38da55ad
fzardo <f.zardo@outlook.com>
2025-01-18 13:59:58 -0300

    week02

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,7 @@
+// Dynamically set the current year in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Dynamically set the last modified date in the footer
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;

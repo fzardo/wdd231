b98a14c8ce4a7ee0d05ac8d933a7e4941365b263
fzardo <f.zardo@outlook.com>
2025-01-19 13:55:42 -0300

    week02

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -6,6 +6,7 @@ document.getElementById('currentyear').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
 
+//Hamburger menu
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
@@ -38,7 +39,50 @@ if (temperature <= 10 && windSpeed > 4.8) {
     windChill = calculateWindChill(temperature, windSpeed) + " °C";
 }
 
-// Dynamically update the webpage content
+// Dynamically update the webpage weather content
 document.getElementById("temp").textContent = `${temperature} °C`;
 document.getElementById("wind").textContent = `${windSpeed} km/h`;
 document.getElementById("windChill").textContent = windChill;
+
+// Get members asynchronously
+const url = 'https://github.com/fzardo/wdd231/blob/main/chamber/scripts/members.json';
+
+const cards = document.querySelector('#members');
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.members);
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+
+        let businessName = document.createElement('h3');
+        let figCaption = document.createElement("figcaption");
+        let image = document.createElement('img');
+
+        businessName.textContent = member.businessName;
+
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', `${member.businessName}`)
+        figCaption.innerHTML = `
+            Email: ${member.address}<br>
+            Phone: ${member.phone}<br>
+            URL: ${member.url}
+        `;
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '340');
+        image.setAttribute('height', '440');
+
+        card.appendChild(businessName);
+        card.appendChild(figCaption);
+        card.appendChild(image);
+
+        cards.appendChild(card);
+    });
+}
+
+getMemberData();

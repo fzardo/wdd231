833b63d02a0391574f0d51313478d42b8a1af51e
fzardo <f.zardo@outlook.com>
2025-01-19 14:37:06 -0300

    week02

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -15,35 +15,6 @@ hambutton.addEventListener('click', () => {
     hambutton.classList.toggle('show');
 });
 
-// Static temperature and wind speed values (Metric)
-const temperature = 10; // °C
-const windSpeed = 5; // km/h
-
-/**
- * Calculate wind chill factor based on temperature (°C) and wind speed (km/h)
- * Formula: Wind Chill (°C) = 13.12 + 0.6215*T - 11.37*(V^0.16) + 0.3965*T*(V^0.16)
- * where T is the air temperature in °C and V is wind speed in km/h.
- */
-function calculateWindChill(temp, wind) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(wind, 0.16) +
-        0.3965 * temp * Math.pow(wind, 0.16)
-    ).toFixed(1); // Return result rounded to one decimal place
-}
-
-// Check if conditions are met for viable wind chill calculation
-let windChill = "N/A";
-if (temperature <= 10 && windSpeed > 4.8) {
-    windChill = calculateWindChill(temperature, windSpeed) + " °C";
-}
-
-// Dynamically update the webpage weather content
-document.getElementById("temp").textContent = `${temperature} °C`;
-document.getElementById("wind").textContent = `${windSpeed} km/h`;
-document.getElementById("windChill").textContent = windChill;
-
 // Get members asynchronously
 const url = 'https://fzardo.github.io/wdd231/chamber/scripts/members.json';
 
